36eeae847fc2241fabb766c6fb1da01fda4c3ce5
Serge Aleynikov <saleyn@gmail.com>
2026-04-08 09:00:38 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExAlign.MixProject do
   def project do
     [
       app: :exalign,
-      version: "0.1.2",
+      version: "0.1.3",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: "A Mix formatter plugin that column-aligns Elixir code",
